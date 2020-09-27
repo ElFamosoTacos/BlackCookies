6b4e27993747654d95271e8c7274aac20369ee2e
ElFamosoTacos <50580410+ElFamosoTacos@users.noreply.github.com>
2020-09-27 14:43:45 +0200

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -1,16 +1,27 @@
 if(document.location.href=="https://www.twitch.tv/"){
-	if(localStorage.getItem("twilight.theme")!="1"){			// if dark theme isn't enabled then
-		localStorage.setItem("twilight.theme", "1");			// enable it
-		document.location.reload();					// reload page
+	if(localStorage.getItem("twilight.theme")!="1"){		// If not dark theme then
+			localStorage.setItem("twilight.theme", "1");	// Set it to be dark theme
+			document.location.reload();						// Reload
 	}
 }
 
-if(document.location.href=="https://www.youtube.com/"){
-	if(localStorage.getItem("ok")!=1){						// if the cookie ok does not exist then
-		document.addEventListener("afterscriptexecute",(event) => {		// wait for script generated pages to load (the one that ask to connect)
-			localStorage.setItem("ok", "1");				// put the cookie that avoid constant reloading
-			document.cookie = "PREF=f6=400;domain=.youtube.com";		// put/set the cookie that enable dark theme
-			document.location.reload();					// reload
-		});
+if((document.location.href=="https://www.youtube.com/") || (document.location.href=="https://www.youtube.com/?gl=FR&tab=m1")){
+
+	if(localStorage.getItem("ok")!="1"){
+		
+		var listCookies = document.cookie.split(';');				// Get all the cookies
+		
+		if(listCookies.length > 5){										// If there is more than 5 cookies, it means that a user is logged and therefore there is no script generated popup
+			document.cookie = "PREF=f6=400;domain=.youtube.com";		// Put/set the cookie that enable dark theme
+			localStorage.setItem("ok", "1");							// Put the cookie that avoid constant reloading
+			document.location.reload();									// Reload
+		}
+		else{
+			document.addEventListener("afterscriptexecute",(event) => {		// Wait for script generated pages to load (the one that ask to connect)
+				localStorage.setItem("ok", "1");							// Put the cookie that avoid constant reloading
+				document.cookie = "PREF=f6=400;domain=.youtube.com";		// Put/set the cookie that enable dark theme
+				document.location.reload();									// Reload
+			});
+		}
 	}
 }
