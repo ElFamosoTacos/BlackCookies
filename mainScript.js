1925802db899c9b85abf3bd656d9ca33194ec828
ElFamosoTacos <50580410+ElFamosoTacos@users.noreply.github.com>
2020-09-27 13:07:25 +0200

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -1,16 +1,16 @@
 if(document.location.href=="https://www.twitch.tv/"){
-	if(localStorage.getItem("twilight.theme")!="1"){	// if not dark theme then
-			localStorage.setItem("twilight.theme", "1");	// set it to be dark theme
-			document.location.reload();						// reload
+	if(localStorage.getItem("twilight.theme")!="1"){			// if dark theme isn't enabled then
+		localStorage.setItem("twilight.theme", "1");			// enable it
+		document.location.reload();					// reload page
 	}
 }
 
 if(document.location.href=="https://www.youtube.com/"){
-	if(localStorage.getItem("ok")!=1){									// if the cookie ok does not exist then
+	if(localStorage.getItem("ok")!=1){						// if the cookie ok does not exist then
 		document.addEventListener("afterscriptexecute",(event) => {		// wait for script generated pages to load (the one that ask to connect)
-			localStorage.setItem("ok", "1");							// put the cookie that avoid constant reloading
+			localStorage.setItem("ok", "1");				// put the cookie that avoid constant reloading
 			document.cookie = "PREF=f6=400;domain=.youtube.com";		// put/set the cookie that enable dark theme
-			document.location.reload();									// reload
+			document.location.reload();					// reload
 		});
 	}
 }
